b639f1493c6722b1ba72fc5b7dcd1f5bba77ca36
Strafred <bred7355608@gmail.com>
2023-05-28 03:55:52 +0700

    delete empty black spaces after updating

--- contentScript.js
+++ contentScript.js
@@ -3,11 +3,16 @@
     chrome.runtime.onMessage.addListener((obj) => {
         const {type} = obj;
 
+        const movieLines = document.getElementsByClassName("ListItem_wrapper__vOmuh");
+        const movieLineInitialSize = movieLines[0].childNodes.length;
+        console.log(movieLineInitialSize);
+
         if (type === "kinopoisk_page_loaded") {
             const movieCards = document.getElementsByClassName("ListItem_selection-card__MeC9v");
-            const movieCardsInitialLength = movieCards.length.valueOf();
 
-            for (let i = 0; i < movieCardsInitialLength; i++) {
+            for (let i = 0; i < movieCards.length; i++) {
+                console.log(i);
+                console.log(movieCards.length);
                 let movieContainer = movieCards[i].getElementsByClassName("SelectionCard_badges-container__zFU8Z")[0];
                 let movieRatingParent = movieContainer.firstChild.firstChild;
                 let movieRatingChild = movieContainer.firstChild.firstChild.firstChild;
@@ -17,6 +22,23 @@
                     if (parseFloat(movieRatingChild.textContent) < 7.0) {
                         movieCards[i].remove();
                         i--;
+                    }
+                }
+            }
+
+            console.log(movieLines.length);
+
+            const movieCardsUpdated = Array.from(movieCards);
+            for (let i = 0; i < movieLines.length; i++) {
+                movieLines[i].childNodes.forEach((node) => {
+                    console.log(node);
+                    node.remove();
+                });
+
+                for (let j = 0; j < movieLineInitialSize; j++) {
+                    if (movieCardsUpdated.length > 0) {
+                        movieLines[i].appendChild(movieCardsUpdated[0]);
+                        movieCardsUpdated.shift();
                     }
                 }
             }
